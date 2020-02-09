800657a49384a6387b8967368bd33ad136eb08ed
lb@macbook <44640570@qq.com>
2020-02-09 17:44:40 +0800

    improve the outcome display in each loop by introducing the getShortResult function, which use sprintf and strncat from <string.h> to assemble result string. To improve the overall efficiency, I use a global static char pResult[200] to handle the result string for each display; therefore avoiding the need to claim for new stack memory each time we need to display something or transfer the pointer through functions calls as arguments.

--- sumtest.c
+++ sumtest.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h> /* use the rand function */
+#include<string.h>
 #define UP_BOUND 10 /* the smallest integer for the addition test */
 #define LOW_BOUND 0 /* the largest integer for the addition test */
 #define BUF_LENGTH 20 /* the MAX length of the excercise */
 
+#define TYPE_1_STR "()+op1=op2"
+#define TYPE_2_STR "op1+()=op2"
+#define TYPE_3_STR "op1+op2=()"
+
+static char pResult[200];
+
 int generateExcercise(char *pExString);
 int resetBuffer(char * pBuf, int length);
+int getShortResult(int iTotal, int iCorrect, char *pRstString);
 
 int main(int argc, char*argv[])
 {
@@ -47,12 +55,25 @@ int main(int argc, char*argv[])
       iRound++;
       iCorrect++;
       fCorrectRatio= 1.00*iCorrect/iRound;
-      printf("恭喜果果，答对了！\n总题目数：%d,做对题目数：%d,正确率是：%f\n\n\n",iRound,iCorrect,fCorrectRatio);
+      //printf("恭喜果果，答对了！\n总题目数：%d,做对题目数：%d,正确率是：%f\n\n\n",iRound,iCorrect,fCorrectRatio);
+      printf("恭喜果果，答对了！\n");
+      if(getShortResult(iRound,iCorrect,pResult)==-1){
+        printf("%s\n",pResult);
+        return -1;
+      }
+      printf("%s",pResult);
     }
     else{
       iRound++;
       fCorrectRatio= 1.00*iCorrect/iRound;
-      printf("额。。。答错了！\n总题目数：%d,做对题目数：%d,正确率是：%f\n\n\n",iRound,iCorrect,fCorrectRatio);
+      /* old version of printing result in each loop */
+      // printf("额。。。答错了！\n总题目数：%d,做对题目数：%d,正确率是：%f\n\n\n",iRound,iCorrect,fCorrectRatio);
+      printf("额。。。答错了！\n");
+      if(getShortResult(iRound,iCorrect,pResult)==-1){
+        printf("%s\n",pResult);
+        return -1;
+      }
+      printf("%s",pResult);
     }
     if(iRound<=20)
       continue;
@@ -70,6 +91,20 @@ int main(int argc, char*argv[])
 
 }
 
+int getShortResult(int iTotal, int iCorrect, char *pRstString)
+{
+  if(iTotal<0 || iCorrect<0 || iTotal<iCorrect){
+    sprintf(pRstString,"illegal aruments input for getShortResult function call");
+    return -1;
+  }
+  char pStatistics[100];
+  //char line[]="---------------------------------------\n";
+  sprintf(pRstString,"-------------------------------------------------\n");
+  sprintf(pStatistics,"总题数：%d\t做对数：%d\t正确率：%.2f%%\n",iTotal,iCorrect,100.00*iCorrect/iTotal);
+  pRstString= strncat(pRstString,pStatistics,100);
+  pRstString= strncat(pRstString,"-------------------------------------------------\n",100);
+  return 0;
+}
 
 int resetBuffer(char * pBuf, int length)
 {
